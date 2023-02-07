dee5b7dad844de13af2eb94814ad09bc8515d7f0
sssktk <s0takt0k@yahoo.co.jp>
2023-02-07 11:13:32 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sota Kataoka on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
