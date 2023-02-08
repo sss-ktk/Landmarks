e570b15369e25d70979467555f45e144b87b42af
sssktk <s0takt0k@yahoo.co.jp>
2023-02-08 16:03:32 -0500

    added Favorite function(toggle button and data) to mark certain Landmarks as your favorite.

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Sota Kataoka on 2/8/23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button{
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
