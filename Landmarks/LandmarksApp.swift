739891d9da8305edd3e73df5b4ea8972a07bc9db
sssktk <s0takt0k@yahoo.co.jp>
2023-02-08 15:49:01 -0500

    adopts the view based on the data store from the ModelData object

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    /* @StateObejct : attribute to initialize a model object for a given property
+       only once during the lift time of the app
+     */
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(ModelData())
         }
     }
 }
