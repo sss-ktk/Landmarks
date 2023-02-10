fd48eaf153c3ccf9586283c0a1b9977782ef900e
sssktk <s0takt0k@yahoo.co.jp>
2023-02-10 17:03:42 -0500

    Edit page can now edit the Dates, Seasonal Photo, and ON/OFF notifications

--- Landmarks/Views/Profile/ProfileHost.swift
+++ Landmarks/Views/Profile/ProfileHost.swift
@@ -21,7 +21,7 @@ struct ProfileHost: View {
             if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
-                Text("Profile Editor")
+                ProfileEditor(profile: $draftProfile)
             }
         }
         .padding()
